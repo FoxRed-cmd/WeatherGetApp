e9e5c7c113a9e0a30bec01731ff6e2443e62edc5
Кирилл Блинов <blinovk955@gmail.com>
2022-05-26 17:05:31 +0300

    Fix Caret Color

--- WeatherGetApp/HelperClasses/ColorHSV.cs
+++ WeatherGetApp/HelperClasses/ColorHSV.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Windows.Media;
-
-namespace WeatherGetApp.HelperClasses
+﻿namespace WeatherGetApp.HelperClasses
 {
     internal struct ColorHSL
     {
@@ -22,49 +18,49 @@ namespace WeatherGetApp.HelperClasses
             };
         }
 
-		public static System.Windows.Media.Color HSLToRGB(ColorHSL hsl, byte alpha)
-		{
-			byte r = 0;
-			byte g = 0;
-			byte b = 0;
+        public static System.Windows.Media.Color HSLToRGB(ColorHSL hsl, byte alpha)
+        {
+            byte r = 0;
+            byte g = 0;
+            byte b = 0;
 
-			if (hsl.Saturation == 0)
-			{
-				r = g = b = (byte)(hsl.Lightness * 255);
-			}
-			else
-			{
-				float v1, v2;
-				float hue = (float)hsl.Hue / 360;
+            if (hsl.Saturation == 0)
+            {
+                r = g = b = (byte)(hsl.Lightness * 255);
+            }
+            else
+            {
+                float v1, v2;
+                float hue = (float)hsl.Hue / 360;
 
-				v2 = (hsl.Lightness < 0.5) ? (hsl.Lightness * (1 + hsl.Saturation)) : ((hsl.Lightness + hsl.Saturation) - (hsl.Lightness * hsl.Saturation));
-				v1 = 2 * hsl.Lightness - v2;
+                v2 = (hsl.Lightness < 0.5) ? (hsl.Lightness * (1 + hsl.Saturation)) : ((hsl.Lightness + hsl.Saturation) - (hsl.Lightness * hsl.Saturation));
+                v1 = 2 * hsl.Lightness - v2;
 
-				r = (byte)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
-				g = (byte)(255 * HueToRGB(v1, v2, hue));
-				b = (byte)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
-			}
+                r = (byte)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+                g = (byte)(255 * HueToRGB(v1, v2, hue));
+                b = (byte)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+            }
 
-			return System.Windows.Media.Color.FromArgb(alpha, r, g, b);
-		}
-		private static float HueToRGB(float v1, float v2, float vH)
-		{
-			if (vH < 0)
-				vH += 1;
+            return System.Windows.Media.Color.FromArgb(alpha, r, g, b);
+        }
+        private static float HueToRGB(float v1, float v2, float vH)
+        {
+            if (vH < 0)
+                vH += 1;
 
-			if (vH > 1)
-				vH -= 1;
+            if (vH > 1)
+                vH -= 1;
 
-			if ((6 * vH) < 1)
-				return (v1 + (v2 - v1) * 6 * vH);
+            if ((6 * vH) < 1)
+                return (v1 + (v2 - v1) * 6 * vH);
 
-			if ((2 * vH) < 1)
-				return v2;
+            if ((2 * vH) < 1)
+                return v2;
 
-			if ((3 * vH) < 2)
-				return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+            if ((3 * vH) < 2)
+                return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
 
-			return v1;
-		}
-	}
+            return v1;
+        }
+    }
 }
